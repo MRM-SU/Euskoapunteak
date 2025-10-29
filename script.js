4286a69553f32333c9da469ac9516afa6cdbe101
MRM-SU <rosco.markel@salesianosurnieta.com>
2025-10-29 07:38:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', async (event) => {
     let spreadsheet = await fetch('https://sheets.googleapis.com/v4/spreadsheets/1OtOYmlJOY9b4onvYjBtL21585g_DaaHWiqLH6oIlp_g/values/bdd?key=AIzaSyDwRrD670SxWVqtuPmHpVyb5PxoptznkY4')
-        .then(response => response.json())
-        .then(json => json.values);
+        .then(response => response.json());
     
     spreadsheet.values.slice(1).forEach(([time,file,tags,gmail]) => {
         file.split(', ').forEach((url) => {
