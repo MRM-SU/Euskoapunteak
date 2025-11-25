c7738da3b7e3d66b082776ac3f7434d48802e983
MRM-SU <rosco.markel@salesianosurnieta.com>
2025-11-25 11:29:39 +0000

    update

---
+++ document.js
@@ -0,0 +1,89 @@
+function getFileID(url) {
+    return url.match(/https\:\/\/drive\.google\.com\/open\?id\=([^\/]+)/)[1];
+}
+
+function DownloadDrive(url) {
+  let id = getFileID(url)
+  if (id) {
+    window.open(`https://drive.usercontent.google.com/u/0/uc?id=${id}&export=download
+`,'_blank');
+  }
+  return true
+};
+
+
+async function main() {
+  var spreadsheet;
+  var sheet;
+  try {
+    let id = parseInt(window.location.hash.slice(1))
+    if (Number.isNaN(id)) {
+      alert('Invalid ID')
+      return
+    }
+    spreadsheet = await fetch('https://sheets.googleapis.com/v4/spreadsheets/1OtOYmlJOY9b4onvYjBtL21585g_DaaHWiqLH6oIlp_g/values/bdd?key=AIzaSyDwRrD670SxWVqtuPmHpVyb5PxoptznkY4')
+          .then(response => response.json());
+    sheet = spreadsheet.values[id+1];
+    report = await fetch('https://sheets.googleapis.com/v4/spreadsheets/1OtOYmlJOY9b4onvYjBtL21585g_DaaHWiqLH6oIlp_g/values/salaketak?key=AIzaSyDwRrD670SxWVqtuPmHpVyb5PxoptznkY4')
+          .then(response => response.json()).then(response => response.values.slice(1));
+    ban = await fetch('https://sheets.googleapis.com/v4/spreadsheets/1OtOYmlJOY9b4onvYjBtL21585g_DaaHWiqLH6oIlp_g/values/ban?key=AIzaSyDwRrD670SxWVqtuPmHpVyb5PxoptznkY4')
+          .then(response => response.json()).then(response => response.values.slice(1));
+    ok = true;
+    report.forEach(_ => {
+      if (_[2] == id) {
+        throw new Error('Apunte hau salatu edo jetsi egin da, ez dago eskuragarri');
+        ok = false;
+      }
+    })
+    ban.forEach(_ => {
+      console.log(parseInt(_[1]) >= 3,_,sheet[3])
+      if (parseInt(_[1]) >= 3 && _[0] == sheet[3]) {
+        throw new Error('Erabiltzaile honek erabilera debekatuta du');
+        ok = false;
+      }
+    })
+    if (!ok) return;
+    console.warn(sheet)
+    if (sheet == undefined) {
+      throw new Error('Invalid ID');
+    }
+  } catch ( error ) {
+    document.body.innerHTML = `<h1>Error</h1><h3>${error.message}</h3><button onclick="window.close();window.location.href='./'">Bueltatu</button>`
+    document.body.classList.add('error');
+    return
+  } finally {
+    document.querySelector('#title').textContent = sheet[4];
+    document.querySelector('#sub').textContent = sheet[5];
+    sheet[2].split(',').forEach(tag => {
+      let tag_ = document.querySelector('#title').appendChild(document.createElement('span'));
+      tag_.id = 'tag';
+      tag_.textContent = tag.trim();
+    })
+    let gmail_ = document.body.appendChild(document.createElement('span'));
+    gmail_.id = 'gmail';
+    gmail_.textContent = sheet[3];
+    gmail_.onclick = () => window.open(`mailto:${sheet[3]}`);
+    gmail_.title = 'Mezu bat bidali'
+
+    let download_ = document.body.appendChild(document.createElement('button'));
+    download_.textContent = `Deskargatu ${sheet[1].split(',').length} fitxategi`;
+    download_.onclick = ()=> sheet[1].split(',').forEach(DownloadDrive);
+
+    document.body.appendChild(document.createElement('br'))
+    document.body.appendChild(document.createElement('h3')).textContent = 'Fitxategiak'
+    sheet[1].split(',').forEach(async url => {
+      let a = document.body.appendChild(document.createElement('a'));
+
+
+
+      let cURL = 'https://www.googleapis.com/drive/v3/files/'+getFileID(url)+'?fields=name&key=AIzaSyDwRrD670SxWVqtuPmHpVyb5PxoptznkY4';
+      console.log(cURL);
+      let li = a.appendChild(document.createElement('li'));
+      li.textContent = await fetch(cURL).then(response => response.json()).then(data => data.name);
+      
+      a.href = url;
+    })
+  }
+}
+
+main()
--- script.js
+++ script.js
@@ -11,24 +11,78 @@ function DownloadDrive(url) {
     return true
 };
 
-document.addEventListener('DOMContentLoaded', async (event) => {
-    let spreadsheet = await fetch('https://sheets.googleapis.com/v4/spreadsheets/1OtOYmlJOY9b4onvYjBtL21585g_DaaHWiqLH6oIlp_g/values/bdd?key=AIzaSyDwRrD670SxWVqtuPmHpVyb5PxoptznkY4')
-        .then(response => response.json());
-    
-    spreadsheet.values.slice(1).forEach(async ([time,files,tags,gmail,title,description]) => {
+var spreadsheet;
 
-        let btn = document.body.appendChild(document.createElement('button'))
-        btn.textContent = 'Descargar todo';
-        btn.onclick = (e) => {
-            files.split(', ').forEach((url) => {
-                DownloadDrive(url)
-            })
-        }
-        let title = document.body.appendChild(document.createElement('h1'));
-        title.textcontent = title
-        let pfp = document.body.appendChild(document.createElement('div'));
+var posts;
+var fuse;
+
+function search_() {
+    const query = document.querySelector('#search input').value;
+    const fuseResults = fuse.search(query);
+    const matchedSet = new Set(fuseResults.map(r => r.item));
+    const matchesBoolean = posts.map(p => matchedSet.has(p));
+    console.log(matchesBoolean);
+    document.querySelectorAll('#pub').forEach((pub, index) => {
+        pub.style.display = matchesBoolean[index] ? 'flex' : 'none';
+    })
+}
+
+document.addEventListener('DOMContentLoaded', async (event) => {
+    document.querySelector('#search input').onchange = search_;
+    document.querySelector('#search button').onclick = search_;
+    spreadsheet = await fetch('https://sheets.googleapis.com/v4/spreadsheets/1OtOYmlJOY9b4onvYjBtL21585g_DaaHWiqLH6oIlp_g/values/bdd?key=AIzaSyDwRrD670SxWVqtuPmHpVyb5PxoptznkY4')
+        .then(response => response.json());
+
+    posts = spreadsheet.values.slice(1).map(([time, files, tags, gmail, title, description]) => ({
+          time,
+          files,
+          tags,
+          gmail,
+          title,
+          description
+        }));
+        fuse = new Fuse(posts, {
+          keys: ["title", "description", "tags"],
+          threshold: 0.3
+        });
+    
+    spreadsheet.values.slice(1).forEach(async ([time,files,tags,gmail,title,description],index) => {
+        let pub = document.body.appendChild(document.createElement('div'));
+        pub.id = 'pub';
+        pub.style.cursor = 'pointer';
+        pub.onclick = () => window.open(`./document.html#${index}`);
+
+        let main = document.createElement('div');
+        main.id = 'main';
+        pub.appendChild(main);
+
+        let top = document.createElement('div');
+        top.id = 'top';
+        main.appendChild(top);
+
+        let tags_ = document.createElement('div');
+        tags_.id = 'tags';
+        top.appendChild(tags_);
+
+        console.log({time,files,tags,gmail,title,description})
+        console.log(title)
+
+        let title_ = top.appendChild(document.createElement('h1'));
+        title_.textContent = title
+        let description_ = main.appendChild(document.createElement('p'));
+        description_.textContent = description
+
+        tags.split(',').forEach(tag => {
+            let tag_ = tags_.appendChild(document.createElement('span'));
+            tag_.textContent = tag.trim();
+            tag_.classList.add('tag');
+            tag_.id = 'tag';
+        })
+
+        let pfp = pub.appendChild(document.createElement('div'));
+        pfp.id = 'pfp';
         const user = gmail.split('@')[0];
-        pfp.style.setProperty('--col',Array.from(user).map((_,i)=>user.charCodeAt(i)).reduce((acc,val)=>acc+val,0))
-        pfp.innerHTML = await fetch('pfp.svg').then(response => response.text())
+        const span = pfp.appendChild(document.createElement('span'));
+        span.textContent = user[0].toUpperCase();
     })
 })
