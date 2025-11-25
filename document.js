5d128f7c5ad40890433f1c4c6206b43c34da1f96
MRM-SU <rosco.markel@salesianosurnieta.com>
2025-11-25 21:18:30 +0100

    update

--- document.js
+++ document.js
@@ -48,7 +48,7 @@ async function main() {
       throw new Error('Invalid ID');
     }
   } catch ( error ) {
-    document.body.innerHTML = `<h1>Error</h1><h3>${error.message}</h3><button onclick="window.parent.close();window.parent.postMessage({action: "hash",hash: ''}, "*");">Bueltatu</button>`
+    document.body.innerHTML = `<h1>Error</h1><h3>${error.message}</h3><button onclick="window.parent.close();window.parent.postMessage({action: 'hash',hash: ''}, '*');">Bueltatu</button>`
     document.body.classList.add('error');
     return
   } finally {
